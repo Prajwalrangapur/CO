1b85b2b0f50d0e307f530f3ad715a70197a00bc4
Prajwalrangapur <prajwalrangapur@gmail.com>
2026-06-22 23:22:50 +0530

    Add data visualization for California housing dataset
    Visualize California housing data with heatmap and pair plot.

---
+++ 2.py
@@ -0,0 +1,17 @@
+#2
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_california_housing
+
+# Load dataset`1
+df = fetch_california_housing(as_frame=True).frame
+
+# Correlation Matrix Heatmap
+plt.figure(figsize=(8, 6))
+sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
+plt.title("Correlation Matrix")
+plt.show()
+
+# Pair Plot
+sns.pairplot(df)
+plt.show()
